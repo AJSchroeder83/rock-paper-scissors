ea2992771170efc8c9fdc2a831485d5f9ab02903
AJSchroeder83 <116520195+AJSchroeder83@users.noreply.github.com>
2022-10-27 20:35:41 +0200

    Start hinzugefügt, Töne nach Schütteln , löschen am Ende

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-let hand = 0
 input.onGesture(Gesture.Shake, function () {
     hand = randint(1, 3)
     if (hand == 1) {
@@ -9,6 +8,7 @@ input.onGesture(Gesture.Shake, function () {
             # . . . #
             # # # # #
             `)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.slide), SoundExpressionPlayMode.UntilDone)
     } else if (hand == 2) {
         basic.showLeds(`
             . . . . .
@@ -17,6 +17,7 @@ input.onGesture(Gesture.Shake, function () {
             . # # # .
             . . . . .
             `)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.twinkle), SoundExpressionPlayMode.UntilDone)
     } else {
         basic.showLeds(`
             # # . . #
@@ -25,8 +26,21 @@ input.onGesture(Gesture.Shake, function () {
             # # . # .
             # # . . #
             `)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
     }
+    basic.pause(5000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
+let hand = 0
+music.playMelody("E B C5 A B G A F ", 200)
+music.playMelody("E B C5 A B G A F ", 200)
+basic.showString("gaming")
 basic.forever(function () {
 	
 })
